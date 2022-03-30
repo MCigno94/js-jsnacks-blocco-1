7d0e8be50f8ae201727881b3a5c134ce13199c3f
MCigno94 <mattiacigno27@gmail.com>
2022-03-30 14:10:17 +0200

    adjustments - SNAK-6

--- snak-6/main.js
+++ snak-6/main.js
@@ -3,12 +3,15 @@ Stampa il cubo dei primi N numeri,
 dove N è un numero indicato dall’utente.
 */
 
-let userNumber;
+let listNumber = ['2', '4', '5', '9', '3', '7', '11'];
+let userNumber = parseInt(prompt(`Inserisci un numero da 1 a ${listNumber.length}`));
+if (userNumber < 1 || userNumber > listNumber.length) {
+    alert('Il numero non esiste a sistema');
+}
 let powNumber;
 
-for (let i = 0; i < 5; i++) {
-    userNumber = parseInt(prompt('Inserisci un numero'));
+for (let i = 0; i < userNumber; i++) {
 
-    powNumber = Math.pow(userNumber, 3);
-    console.log(`n° ${userNumber} - il cubo: ${powNumber}`);
+    powNumber = Math.pow(listNumber[i], 3);
+    console.log(`Il cubo di "${listNumber[i]}" è: ${powNumber}`);
 }
