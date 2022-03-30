994da6c663146c398586a51581151eadd26c8e1b
MCigno94 <mattiacigno27@gmail.com>
2022-03-30 10:29:52 +0200

    snak-1- adjustments

--- snak-1/main.js
+++ snak-1/main.js
@@ -10,4 +10,6 @@ if (utente1 > utente2) {
     console.log(`Il numero più alto è dell'utente n°1: ${utente1}`);
 } else if (utente2 > utente1) {
     console.log(`Il numero più alto è dell'utente n°2: ${utente2}`);
+} else {
+    console.log(`I numeri sono uguali: Utente n°1 ==> ${utente1} <== | Utente n°2 ==> ${utente2} <==`);
 }
