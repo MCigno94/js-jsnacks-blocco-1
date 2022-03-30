f0f4c3722a9f81d27ed720cb40beae6cfb57e2f0
MCigno94 <mattiacigno27@gmail.com>
2022-03-30 15:54:17 +0200

    adjustements SNAK-7

--- snak-7/main.js
+++ snak-7/main.js
@@ -5,14 +5,16 @@ Stampa le potenze di 2 fino a 1000.
 let number = 2;
 let pow100;
 let greaterThan1000 = true;
+let indexPow;
 
 for (let i = 0; i <= 20; i++) {
 
     pow100 = parseInt(Math.pow(number, i));
     if (pow100 < 1000) {
         console.log(`${number} elevato a ${i} = ${pow100}`);
+        indexPow = i + 1;
     } else {
         greaterThan1000 = false;
-
     }
 }
+console.log(`${number} elevato a ${indexPow} = stampa un numero maggiore di 1000`);
