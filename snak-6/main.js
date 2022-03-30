13cfffa83937dfda304fb45048c2a402b97778f1
MCigno94 <mattiacigno27@gmail.com>
2022-03-30 14:12:05 +0200

    final commit - SNAK-6

--- snak-6/main.js
+++ snak-6/main.js
@@ -5,13 +5,14 @@ dove N è un numero indicato dall’utente.
 
 let listNumber = ['2', '4', '5', '9', '3', '7', '11'];
 let userNumber = parseInt(prompt(`Inserisci un numero da 1 a ${listNumber.length}`));
+let powNumber;
 if (userNumber < 1 || userNumber > listNumber.length) {
     alert('Il numero non esiste a sistema');
-}
-let powNumber;
+} else {
 
-for (let i = 0; i < userNumber; i++) {
+    for (let i = 0; i < userNumber; i++) {
 
-    powNumber = Math.pow(listNumber[i], 3);
-    console.log(`Il cubo di "${listNumber[i]}" è: ${powNumber}`);
+        powNumber = Math.pow(listNumber[i], 3);
+        console.log(`Il cubo di "${listNumber[i]}" è: ${powNumber}`);
+    }
 }
