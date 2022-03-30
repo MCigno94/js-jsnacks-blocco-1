7eb54d45aacbe5b2a47303b779673fb76688bcb3
MCigno94 <mattiacigno27@gmail.com>
2022-03-30 12:39:17 +0200

    snak-5

---
+++ snak-5/main.js
@@ -0,0 +1,17 @@
+/*
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array.
+*/
+let listNumber = [];
+let userNumber;
+
+for (let i = 0; i < 6; i++) {
+
+    userNumber = parseInt(prompt('Inserisci un numero'));
+    if (userNumber % 2 == 0) {
+        listNumber.push(userNumber);
+    }
+}
+
+console.log(listNumber);
