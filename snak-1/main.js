2b31d2673fdab920fe67d46e8a527dce2d02bc0b
MCigno94 <mattiacigno27@gmail.com>
2022-03-30 10:36:04 +0200

    adjustments-SNAK

--- snak-1/main.js
+++ snak-1/main.js
@@ -4,7 +4,13 @@ Il software stampa il maggiore.
 */
 
 let utente1 = parseInt(prompt('Utente 1, inserisci un numero'));
+if (isNaN(utente1)) {
+    alert('inserisci un numero')
+}
 let utente2 = parseInt(prompt('Utente 2, inserisci un numero'));
+if (isNaN(utente2)) {
+    alert('inserisci un numero')
+}
 
 if (utente1 > utente2) {
     console.log(`Il numero più alto è dell'utente n°1: ${utente1}`);
