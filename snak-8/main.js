8ac5ec2aff84af1f7b56d0655507f65b0f2331ec
MCigno94 <mattiacigno27@gmail.com>
2022-03-30 14:48:54 +0200

    final commit - SNAK-8

--- snak-8/main.js
+++ snak-8/main.js
@@ -3,16 +3,21 @@ Chiedi un numero di 4 cifre all’utente
 e calcola la somma di tutte le cifre che compongono il numero.
 */
 
-let userNumber = '1234';
+let userNumber = String(prompt('Inserisci un numero di 4 cifre'));
 let x;
 let somma = 0;
+if (parseInt(userNumber.length) !== 4) {
+    alert('Il numero non corrisponde')
 
-for (let i = 0; i < userNumber.length; i++) {
-    //console.log(userNumber.substring(i, i + 1));
+} else {
 
-    x = parseInt(userNumber.substring(i, i + 1));
-    somma += x;
+    for (let i = 0; i < userNumber.length; i++) {
+        //console.log(userNumber.substring(i, i + 1));
 
+        x = parseInt(userNumber.substring(i, i + 1));
+        somma += x;
+
+    }
 }
 
 console.log(somma);
