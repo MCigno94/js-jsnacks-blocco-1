0637a9b7bda1f85adce4a2901da65393651fbdd5
MCigno94 <mattiacigno27@gmail.com>
2022-03-31 11:40:51 +0200

    snak-3_while

---
+++ snak-3_while/main.js
@@ -0,0 +1,15 @@
+/*
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+*/
+
+let somma = 0;
+let i = 1;
+
+while (i <= 10) {
+    numberUser = Number(prompt('Inserisci un numero'));
+    somma += numberUser;
+    i++;
+}
+
+console.log(`La somma dei 10 numeri inseriti è: ${somma}`);
